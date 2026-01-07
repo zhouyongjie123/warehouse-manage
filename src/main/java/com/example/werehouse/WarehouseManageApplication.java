d9c34da39f0ebc0ace68626bd8e07bb8895d38fa
冰咖啡 <z1876849563@163.com>
2026-01-07 10:26:34 +0800

    init

---
+++ src/main/java/com/example/werehouse/WarehouseManageApplication.java
@@ -0,0 +1,13 @@
+package com.example.werehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarehouseManageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/werehouse/WarehouseManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.werehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
